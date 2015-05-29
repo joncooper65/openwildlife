abc18abf1aceee757f35dfcef504d2623c5637fd
nbnrain <jcoop@ceh.ac.uk>
2015-05-29 14:54:51 +0100

    added better handling of linking vagrant folder where the app is to apache's www folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,10 @@
 echo "About to install Apache2"
 apt-get update
 apt-get install -y apache2
-if ! [ -L /var/www ]; then
+if [ -d /var/www ]; then
   rm -rf /var/www
-  ln -fs /vagrant /var/www/html
+  echo 'cleaned out /var/www'
 fi
+mkdir /var/www
+ln -fs /vagrant /var/www/html
+echo 'pointed /var/www/html at /vagrant'
